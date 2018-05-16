19df5e86b9c36b041a3505fa9717f7fd6f461dee
Giedrė Genutė <giedre.genute@gmail.com>
2018-05-16 13:48:21 +0300

    Changed js file name

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,19 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
+
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+
     .cleanupOutputBeforeBuild()
+
     .enableSourceMaps(true)
+
     // uncomment to create hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(true)
+    .enableVersioning()
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    .addEntry('js/landing', './assets/js/landing.js')
+    .addEntry('js/inside', './assets/js/inside.js')
     .addStyleEntry('css/inside', './assets/css/inside.scss')
     .addStyleEntry('css/landing', './assets/css/landing.scss')
 
@@ -20,7 +23,7 @@ Encore
     .enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    //.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
