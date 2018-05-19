19ee3580d9c2e0eb2a8696c4d9e67b1607924c13
Dangyras Stepanavicius <omnus@windowslive.com>
2018-05-19 17:46:40 +0300

    New entities

---
+++ src/Entity/Document.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DocumentRepository")
+ */
+class Document
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $documentName;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $documentDate;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $documentReminder;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $documentExpires;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $documentNotes;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="documents")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="documents")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Tag", inversedBy="documents")
+     */
+    private $tags;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\File", mappedBy="document")
+     */
+    private $files;
+
+    public function __construct()
+    {
+        $this->tags = new ArrayCollection();
+        $this->files = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getDocumentName(): ?string
+    {
+        return $this->documentName;
+    }
+
+    public function setDocumentName(string $documentName): self
+    {
+        $this->documentName = $documentName;
+
+        return $this;
+    }
+
+    public function getDocumentDate(): ?\DateTimeInterface
+    {
+        return $this->documentDate;
+    }
+
+    public function setDocumentDate(\DateTimeInterface $documentDate): self
+    {
+        $this->documentDate = $documentDate;
+
+        return $this;
+    }
+
+    public function getDocumentReminder(): ?\DateTimeInterface
+    {
+        return $this->documentReminder;
+    }
+
+    public function setDocumentReminder(?\DateTimeInterface $documentReminder): self
+    {
+        $this->documentReminder = $documentReminder;
+
+        return $this;
+    }
+
+    public function getDocumentExpires(): ?\DateTimeInterface
+    {
+        return $this->documentExpires;
+    }
+
+    public function setDocumentExpires(?\DateTimeInterface $documentExpires): self
+    {
+        $this->documentExpires = $documentExpires;
+
+        return $this;
+    }
+
+    public function getDocumentNotes(): ?string
+    {
+        return $this->documentNotes;
+    }
+
+    public function setDocumentNotes(string $documentNotes): self
+    {
+        $this->documentNotes = $documentNotes;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Tag[]
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags[] = $tag;
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        if ($this->tags->contains($tag)) {
+            $this->tags->removeElement($tag);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|File[]
+     */
+    public function getFiles(): Collection
+    {
+        return $this->files;
+    }
+
+    public function addFile(File $file): self
+    {
+        if (!$this->files->contains($file)) {
+            $this->files[] = $file;
+            $file->setDocument($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFile(File $file): self
+    {
+        if ($this->files->contains($file)) {
+            $this->files->removeElement($file);
+            // set the owning side to null (unless already changed)
+            if ($file->getDocument() === $this) {
+                $file->setDocument(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $userName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $userEmail;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $userToken;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Document", mappedBy="user")
+     */
+    private $documents;
+
+    public function __construct()
+    {
+        $this->documents = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->userName;
+    }
+
+    public function setUserName(string $userName): self
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    public function getUserEmail(): ?string
+    {
+        return $this->userEmail;
+    }
+
+    public function setUserEmail(string $userEmail): self
+    {
+        $this->userEmail = $userEmail;
+
+        return $this;
+    }
+
+    public function getUserToken(): ?string
+    {
+        return $this->userToken;
+    }
+
+    public function setUserToken(string $userToken): self
+    {
+        $this->userToken = $userToken;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Document[]
+     */
+    public function getDocuments(): Collection
+    {
+        return $this->documents;
+    }
+
+    public function addDocument(Document $document): self
+    {
+        if (!$this->documents->contains($document)) {
+            $this->documents[] = $document;
+            $document->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDocument(Document $document): self
+    {
+        if ($this->documents->contains($document)) {
+            $this->documents->removeElement($document);
+            // set the owning side to null (unless already changed)
+            if ($document->getUser() === $this) {
+                $document->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+}
