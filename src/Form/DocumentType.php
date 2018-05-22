afc251f12215185c4d452116ba9a290eb7109fd5
Dangyras Stepanavicius <omnus@windowslive.com>
2018-05-22 10:31:28 +0300

    Added forms.

--- src/Form/DocumentType.php
+++ src/Form/DocumentType.php
@@ -8,16 +8,18 @@
 
 namespace App\Form;
 
-
 use App\Entity\Category;
+use App\Entity\Document;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DocumentType extends AbstractType
 {
@@ -43,6 +45,10 @@ class DocumentType extends AbstractType
                 'attr' => array(),
                 'required' => false
             ))
+//            ->add('tag', CollectionType::class, array(
+//                'entry_type' => TagType::class,
+//                'entry_options' => array('label' => false),
+//            ))
             ->add("documentNotes", TextareaType::class, array(
                 'attr' => array(),
                 'required' => false
@@ -59,5 +65,12 @@ class DocumentType extends AbstractType
                 'attr' => array("onClick" => "goBack()")
             ))
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Document::class,
+        ));
     }
 }
---
+++ src/Form/TagType.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dangis
+ * Date: 18.5.22
+ * Time: 09.06
+ */
+
+namespace App\Form;
+
+use App\Entity\Tag;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TagType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('tagName');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Tag::class,
+        ));
+    }
+}
